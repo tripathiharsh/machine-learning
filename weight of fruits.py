2c6e8cc2fa7da2f5682eafa6e88b200183376db2
tripathiharsh <77981237+tripathiharsh@users.noreply.github.com>
2021-02-27 16:46:27 +0530

    Create weight of fruits.py

---
+++ weight of fruits.py
@@ -0,0 +1,10 @@
+from sklearn import tree
+
+features=[[120 ],[160],[30]]
+labels=["apple","banana","grapes"]
+clf=tree.DecisionTreeClassifier()
+clf=clf.fit(features,labels)
+s=int(input("weight >"))
+
+print (clf.predict([[s]]))
+	
